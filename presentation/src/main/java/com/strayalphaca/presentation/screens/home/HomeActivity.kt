7672c092c7a3d0b5eb5646094d98b465a3dcc821
yunsehwan <sh10302@naver.com>
2023-03-01 22:17:21 +0900

    first commit

---
+++ presentation/src/main/java/com/strayalphaca/presentation/screens/home/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.strayalphaca.presentation.screens.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("Hello world!")
+        }
+    }
+}
