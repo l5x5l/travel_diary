755d159ee1040d93130ee840f97fcc205ac10f6b
yunsehwan <sh10302@naver.com>
2023-03-01 22:36:07 +0900

    [setting] 모듈 생성 및 라이브러리 세팅

---
+++ app/src/main/java/com/strayalphaca/travel_diary/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.strayalphaca.travel_diary
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
---
+++ domain/src/main/java/com/strayalphaca/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.strayalphaca.domain
+
+class MyClass {
+}
