3ca99d1e7166c09b02f57588ead805ecd63d65bb
yunsehwan <sh10302@naver.com>
2023-06-21 16:34:57 +0900

    [feature] 비디오 썸네일 이미지 view 생성 및 적용

--- presentation/src/main/java/com/strayalphaca/presentation/components/block/PolaroidView.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/components/block/PolaroidView.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,61 +12,62 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import com.strayalphaca.presentation.components.atom.video_thumbnail_image.VideoThumbnailImage
 import com.strayalphaca.presentation.ui.theme.Gray4
 import com.strayalphaca.presentation.ui.theme.TravelDiaryTheme
 import com.strayalphaca.presentation.utils.decodeSampledBitmapFromResource
 import com.strayalphaca.presentation.utils.getFileFromUri
 
-// todo 동영상인 경우 썸네일 이미지 표시 및 위에 재생 버튼 표시
 @Composable
 fun PolaroidView(
-    fileUri : Uri = Uri.EMPTY,
-    isVideo : Boolean = false,
-    dateString : String = "2023.02.10_18:34",
-    positionString : String = "1/1",
-    onClick : (Uri) -> Unit = {}
+    fileUri: Uri = Uri.EMPTY,
+    isVideo: Boolean = false,
+    dateString: String = "2023.02.10_18:34",
+    positionString: String = "1/1",
+    onClick: (Uri) -> Unit = {}
 ) {
     val imageViewSize = remember { mutableStateOf(IntSize(0, 0)) }
     val context = LocalContext.current
 
     Box(modifier = Modifier.fillMaxWidth()) {
-        Surface(modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-            .shadow(elevation = 6.dp)
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .shadow(elevation = 6.dp)
         ) {
             Column(modifier = Modifier.padding(12.dp)) {
                 if (isVideo) {
-                    Image(modifier = Modifier
-                        .fillMaxWidth()
-                        .height(280.dp)
-                        .background(Color.Cyan)
-                        .onGloballyPositioned { coordinates ->
-                            imageViewSize.value = coordinates.size
-                        }
-                        .clickable {
-                           onClick(fileUri)
-                        },
-                        bitmap = ImageBitmap(280, 280),
-                        contentDescription = "diary image",
-                        contentScale = ContentScale.Crop
+                    VideoThumbnailImage(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(280.dp)
+                            .onGloballyPositioned { coordinates ->
+                                imageViewSize.value = coordinates.size
+                            },
+                        videoUri = fileUri,
+                        onClick = { onClick(fileUri) }
                     )
                 } else {
-                    Image(modifier = Modifier
-                        .fillMaxWidth()
-                        .height(280.dp)
-                        .background(Color.Cyan)
-                        .onGloballyPositioned { coordinates ->
-                            imageViewSize.value = coordinates.size
-                        },
-                        bitmap = decodeSampledBitmapFromResource(getFileFromUri(fileUri, context) ?: byteArrayOf(), imageViewSize.value.width, imageViewSize.value.height),
+                    Image(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(280.dp)
+                            .background(Color.Cyan)
+                            .onGloballyPositioned { coordinates ->
+                                imageViewSize.value = coordinates.size
+                            },
+                        bitmap = decodeSampledBitmapFromResource(
+                            getFileFromUri(fileUri, context) ?: byteArrayOf(),
+                            imageViewSize.value.width,
+                            imageViewSize.value.height
+                        ),
                         contentDescription = "diary image",
                         contentScale = ContentScale.Crop
                     )
@@ -77,9 +77,14 @@ fun PolaroidView(
 
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Text(text = dateString, style = MaterialTheme.typography.caption, color = Gray4)
-                    Text(text = positionString, style = MaterialTheme.typography.caption, color = Gray4)
+                    Text(
+                        text = positionString,
+                        style = MaterialTheme.typography.caption,
+                        color = Gray4
+                    )
                 }
             }
         }
@@ -98,7 +103,7 @@ fun PolaroidView(
 fun PolaroidViewPreview() {
     TravelDiaryTheme {
         Surface(modifier = Modifier.padding(16.dp)) {
-            
+
             PolaroidView()
         }
     }
--- presentation/src/main/java/com/strayalphaca/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/ui/theme/Color.kt
@@ -12,3 +12,5 @@ val Gray2 = Color(0xFFEEEEEE)
 val Gray4 = Color(0xFF7D7D7D)
 
 val GradientGray = Color(0x407D7D7D)
+
+val BlackSurfaceTransparent50 = Color(0x802A2B2C)
