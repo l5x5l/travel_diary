461a34dc9c9a00ed9acb4dbbed52a59aea5df11f
yunsehwan <sh10302@naver.com>
2023-07-27 17:00:22 +0900

    [feature] map 관련 data 모듈 추가

---
+++ data/map/src/main/java/com/strayalphaca/travel_diary/data/map/repository/TestMapRepository.kt
@@ -0,0 +1,29 @@
+package com.strayalphaca.travel_diary.data.map.repository
+
+import com.strayalphaca.travel_diary.map.model.City
+import com.strayalphaca.travel_diary.map.model.CityDiary
+import com.strayalphaca.travel_diary.map.model.Province
+import com.strayalphaca.travel_diary.map.model.ProvinceDiary
+import com.strayalphaca.travel_diary.map.repository.MapRepository
+import javax.inject.Inject
+
+// only for testing
+class TestMapRepository @Inject constructor() : MapRepository {
+    override suspend fun loadProvincePostList(provinceId: Int): List<ProvinceDiary> {
+        return listOf(
+            ProvinceDiary("", Province.Busan),
+            ProvinceDiary("", Province.Sejong),
+            ProvinceDiary("", Province.Seoul),
+            ProvinceDiary("", Province.Daejeon),
+            ProvinceDiary("", Province.Ulsan),
+        )
+    }
+
+    override suspend fun loadCityPostList(cityIdList: List<Int>): List<CityDiary> {
+        return listOf(
+            CityDiary(
+                thumbnailUri = "", City.findCity(26)
+            )
+        )
+    }
+}
