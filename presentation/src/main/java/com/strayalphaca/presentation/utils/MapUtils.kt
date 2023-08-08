bdcde77a8bcc0121431ad531425963b9cec4ad37
yunsehwan <sh10302@naver.com>
2023-08-08 15:32:39 +0900

    [fix] 위치 오타 수정 및 일부 province 데이터 추가

--- presentation/src/main/java/com/strayalphaca/presentation/utils/MapUtils.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/utils/MapUtils.kt
@@ -1,15 +1,23 @@
 package com.strayalphaca.presentation.utils
 
 import com.strayalphaca.presentation.R
+import com.strayalphaca.travel_diary.map.model.PROVINCE_BUSAN
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Chungcheongbuk
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Chungcheongnam
+import com.strayalphaca.travel_diary.map.model.PROVINCE_DAEGU
+import com.strayalphaca.travel_diary.map.model.PROVINCE_DAEJEON
+import com.strayalphaca.travel_diary.map.model.PROVINCE_GWANGJU
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Gangwon
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Gyeonggi
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Gyeongsangbuk
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Gyeongsangnam
+import com.strayalphaca.travel_diary.map.model.PROVINCE_INCHEON
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Jeju
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Jeollabuk
 import com.strayalphaca.travel_diary.map.model.PROVINCE_Jeollanam
+import com.strayalphaca.travel_diary.map.model.PROVINCE_SEJONG
+import com.strayalphaca.travel_diary.map.model.PROVINCE_SEOUL
+import com.strayalphaca.travel_diary.map.model.PROVINCE_ULSAN
 
 
 fun getMapImageResourceById(id: Int?): Int {
@@ -17,7 +25,7 @@ fun getMapImageResourceById(id: Int?): Int {
         null -> {
             R.drawable.ic_map_korea
         }
-        PROVINCE_Gyeonggi -> {
+        PROVINCE_Gyeonggi, PROVINCE_SEOUL, PROVINCE_INCHEON -> {
             R.mipmap.img_map_gyeonggi
         }
         PROVINCE_Gangwon -> {
@@ -26,19 +34,19 @@ fun getMapImageResourceById(id: Int?): Int {
         PROVINCE_Chungcheongbuk -> {
             R.mipmap.img_map_chungbuk
         }
-        PROVINCE_Chungcheongnam -> {
+        PROVINCE_Chungcheongnam, PROVINCE_DAEJEON, PROVINCE_SEJONG -> {
             R.mipmap.img_map_chungnam
         }
         PROVINCE_Jeollabuk -> {
             R.mipmap.img_map_jeonbuk
         }
-        PROVINCE_Jeollanam -> {
+        PROVINCE_Jeollanam, PROVINCE_GWANGJU -> {
             R.mipmap.img_map_jeonnam
         }
-        PROVINCE_Gyeongsangbuk -> {
+        PROVINCE_Gyeongsangbuk, PROVINCE_DAEGU -> {
             R.mipmap.img_map_gyeongbuk
         }
-        PROVINCE_Gyeongsangnam -> {
+        PROVINCE_Gyeongsangnam, PROVINCE_BUSAN, PROVINCE_ULSAN -> {
             R.mipmap.img_map_gyeongnam
         }
         PROVINCE_Jeju -> {
@@ -56,7 +64,7 @@ fun getMapImageRatioById(id : Int?) : Float {
         null -> {
             0.66f
         }
-        PROVINCE_Gyeonggi -> {
+        PROVINCE_Gyeonggi, PROVINCE_SEOUL, PROVINCE_INCHEON -> {
             0.74f
         }
         PROVINCE_Gangwon -> {
@@ -65,19 +73,19 @@ fun getMapImageRatioById(id : Int?) : Float {
         PROVINCE_Chungcheongbuk -> {
             0.935f
         }
-        PROVINCE_Chungcheongnam -> {
+        PROVINCE_Chungcheongnam, PROVINCE_DAEJEON, PROVINCE_SEJONG  -> {
             1.035f
         }
         PROVINCE_Jeollabuk -> {
             1.40f
         }
-        PROVINCE_Jeollanam -> {
+        PROVINCE_Jeollanam, PROVINCE_GWANGJU -> {
             1.15f
         }
-        PROVINCE_Gyeongsangbuk -> {
+        PROVINCE_Gyeongsangbuk, PROVINCE_DAEGU -> {
             0.9f
         }
-        PROVINCE_Gyeongsangnam -> {
+        PROVINCE_Gyeongsangnam, PROVINCE_BUSAN, PROVINCE_ULSAN -> {
             1.26f
         }
         PROVINCE_Jeju -> {
