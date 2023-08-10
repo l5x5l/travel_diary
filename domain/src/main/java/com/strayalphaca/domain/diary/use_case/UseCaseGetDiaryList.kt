f4ddc63d590d788ace633fbdc5f40432a620e29c
yunsehwan <sh10302@naver.com>
2023-08-10 21:05:18 +0900

    [feature] 기록목록 조회 useCase 생성

---
+++ domain/src/main/java/com/strayalphaca/domain/diary/use_case/UseCaseGetDiaryList.kt
@@ -0,0 +1,13 @@
+package com.strayalphaca.domain.diary.use_case
+
+import com.strayalphaca.domain.diary.model.DiaryItem
+import com.strayalphaca.domain.diary.repository.DiaryRepository
+import javax.inject.Inject
+
+class UseCaseGetDiaryList @Inject constructor(
+    private val repository: DiaryRepository
+) {
+    suspend operator fun invoke(cityId : Int, perPage : Int, offset : Int) : List<DiaryItem> {
+        return repository.getDiaryList(cityId, perPage, offset)
+    }
+}
