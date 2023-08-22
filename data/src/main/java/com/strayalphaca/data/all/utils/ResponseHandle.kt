4b3de3eca84345382b2245c70cf40beeac616a0e
yunsehwan <sh10302@naver.com>
2023-08-22 21:23:57 +0900

    [feature] retrofit response 처리 관련 유틸 함수 생성

---
+++ data/src/main/java/com/strayalphaca/data/all/utils/ResponseHandle.kt
@@ -0,0 +1,44 @@
+package com.strayalphaca.data.all.utils
+
+import com.strayalphaca.domain.model.BaseResponse
+import retrofit2.Response
+
+fun<T, G> responseToBaseResponseWithMapping(
+    response : Response<T>,
+    mappingFunction : (T) -> G
+) : BaseResponse<G> {
+    val responseBody = response.body()
+    return if (response.isSuccessful && responseBody != null) {
+        BaseResponse.Success(data = mappingFunction(responseBody))
+    } else {
+        BaseResponse.Failure(
+            errorCode = response.code(),
+            errorMessage = response.message()
+        )
+    }
+}
+
+fun<T> responseToBaseResponse(
+    response : Response<T>
+) : BaseResponse<T> {
+    val responseBody = response.body()
+    return if (response.isSuccessful && responseBody != null) {
+        BaseResponse.Success(data = responseBody)
+    } else {
+        BaseResponse.Failure(
+            errorCode = response.code(),
+            errorMessage = response.message()
+        )
+    }
+}
+
+fun voidResponseToBaseResponse(response : Response<Unit>) : BaseResponse<Nothing> {
+    return if (response.isSuccessful) {
+        BaseResponse.EmptySuccess
+    } else {
+        BaseResponse.Failure(
+            errorCode = response.code(),
+            errorMessage = response.message()
+        )
+    }
+}
