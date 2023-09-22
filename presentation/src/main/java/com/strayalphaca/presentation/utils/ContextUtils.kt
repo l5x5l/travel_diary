1b630d1b51df28a002129f7eb82012f5ea80a902
yunsehwan <sh10302@naver.com>
2023-09-22 12:47:13 +0900

    [feature] context를 통해 Activity에 접근하는 함수와 세팅 앱으로 이동하는 함수 구현

--- presentation/src/main/java/com/strayalphaca/presentation/utils/ContextUtils.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/utils/ContextUtils.kt
@@ -1,9 +1,13 @@
 package com.strayalphaca.presentation.utils
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
 
 fun restartApplication(context : Context) {
     val packageManager: PackageManager = context.packageManager
@@ -12,4 +16,23 @@ fun restartApplication(context : Context) {
     val restartIntent: Intent = Intent.makeRestartActivityTask(componentName)
     context.startActivity(restartIntent)
     Runtime.getRuntime().exit(0)
+}
+
+internal fun Context.findActivity() : Activity? {
+    var currentContext = this
+    while (currentContext is ContextWrapper) {
+        if (currentContext is Activity) {
+            return currentContext
+        }
+        currentContext = currentContext.baseContext
+    }
+    return null
+}
+
+
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also (::startActivity)
 }
