d0ffc68b9f3cf8ede4d34d7e7d69c413c661f541
yunsehwan <sh10302@naver.com>
2023-10-08 09:59:35 +0900

    [fix] 달력에 사용되는 DiaryInCalendar의 날짜 관련 변수를 day만 사용하던 방식에서 diaryDate를 사용하는 방식으로 수정

--- domain/calendar/src/main/java/com/strayalphaca/travel_diary/domain/calendar/model/DiaryInCalendar.kt
+++ domain/calendar/src/main/java/com/strayalphaca/travel_diary/domain/calendar/model/DiaryInCalendar.kt
@@ -1,7 +1,11 @@
 package com.strayalphaca.travel_diary.domain.calendar.model
 
+import com.strayalphaca.domain.all.DiaryDate
+
 data class DiaryInCalendar(
     val id : String,
-    val day : Int = 1,
+    val date : DiaryDate,
     val thumbnailUrl : String ?= null
-)
+) {
+    val day : Int = date.day
+}
