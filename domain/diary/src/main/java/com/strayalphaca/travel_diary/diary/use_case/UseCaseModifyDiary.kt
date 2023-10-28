8d9ac1aca2cd94a0b5a7ab33e874ed0be8ee362f
yunsehwan <sh10302@naver.com>
2023-10-28 16:11:00 +0900

    [feature] Diary 작성 관련 에러 코드 정의 및 작성/수정 useCase에 적용

--- domain/diary/src/main/java/com/strayalphaca/travel_diary/diary/use_case/UseCaseModifyDiary.kt
+++ domain/diary/src/main/java/com/strayalphaca/travel_diary/diary/use_case/UseCaseModifyDiary.kt
@@ -3,12 +3,17 @@ package com.strayalphaca.travel_diary.diary.use_case
 import com.strayalphaca.travel_diary.diary.model.DiaryModifyData
 import com.strayalphaca.travel_diary.diary.repository.DiaryRepository
 import com.strayalphaca.domain.model.BaseResponse
+import com.strayalphaca.travel_diary.diary.model.DiaryErrorCodes
 import javax.inject.Inject
 
 class UseCaseModifyDiary @Inject constructor(
     private val repository: DiaryRepository
 ) {
     suspend operator fun invoke(diaryModifyData: DiaryModifyData) : BaseResponse<Nothing> {
+        if (diaryModifyData.content == "") {
+            return BaseResponse.Failure(errorCode = DiaryErrorCodes.EMPTY_ARGUMENT_DIARY_CONTENT, errorMessage = "")
+        }
+
         return repository.modifyDiary(diaryModifyData)
     }
 }
