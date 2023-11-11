407cbdcf0c508f61bc2b17c21ac58bfe14bbc6a8
yunsehwan <sh10302@naver.com>
2023-11-12 00:58:49 +0900

    [fix] paging3를 적용했던 일지 목록 화면을 SimplePaging 구현체를 사용하도록 변경

--- presentation/src/main/java/com/strayalphaca/presentation/screens/diary_list/DiaryListViewModel.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/screens/diary_list/DiaryListViewModel.kt
@@ -2,19 +2,13 @@ package com.strayalphaca.presentation.screens.diary_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
-import com.strayalphaca.travel_diary.diary.model.DiaryItem
+import com.strayalphaca.presentation.screens.diary_list.paging.DiaryListSimplePaging
 import com.strayalphaca.travel_diary.diary.use_case.UseCaseGetDiaryList
-import com.strayalphaca.presentation.screens.diary_list.paging.DiaryListPagingSource
 import com.strayalphaca.travel_diary.map.model.City
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,13 +18,18 @@ class DiaryListViewModel @Inject constructor(
 
     private var cityGroupId : Int = 0
 
-    var diaryPager : Flow<PagingData<DiaryItem>> = emptyFlow()
+    var diaryListSimplePaging = DiaryListSimplePaging(
+        getDiaryList = useCaseGetDiaryList::getByCityGroupId,
+        perPage = 10,
+        targetId = cityGroupId
+    )
+        private set
 
     var selectedCityId : Int ?= null
         private set
 
 
-    private val _locationTitle = MutableStateFlow<String>("-")
+    private val _locationTitle = MutableStateFlow("-")
     val locationTitle = _locationTitle.asStateFlow()
 
     fun setCityGroup(cityGroupId : Int) {
@@ -49,14 +48,33 @@ class DiaryListViewModel @Inject constructor(
                     .joinToString(separator = ",") { city ->
                         city.name
                     }
-            diaryPager = Pager(PagingConfig(pageSize = 10)) {
-                DiaryListPagingSource(useCaseGetDiaryList::getByCityGroupId, 10, cityGroupId)
-            }.flow.cachedIn(viewModelScope)
+
+            diaryListSimplePaging = DiaryListSimplePaging(
+                getDiaryList = useCaseGetDiaryList::getByCityGroupId,
+                perPage = 10,
+                targetId = cityGroupId
+            )
+            viewModelScope.launch {
+                diaryListSimplePaging.refresh()
+            }
+
         } else {
             _locationTitle.value = City.findCity(cityId).name
-            diaryPager = Pager(PagingConfig(pageSize = 10)) {
-                DiaryListPagingSource(useCaseGetDiaryList::invoke, 10, cityId)
-            }.flow.cachedIn(viewModelScope)
+
+            diaryListSimplePaging = DiaryListSimplePaging(
+                getDiaryList = useCaseGetDiaryList::invoke,
+                perPage = 10,
+                targetId = cityId
+            )
+            viewModelScope.launch {
+                diaryListSimplePaging.refresh()
+            }
+        }
+    }
+
+    fun loadNext() {
+        viewModelScope.launch {
+            diaryListSimplePaging.load()
         }
     }
 }
