7f72ae5759ea67b0ac3a3189feaf5f6a00b49c94
yunsehwan <sh10302@naver.com>
2023-11-15 11:04:34 +0900

    [fix] 로그아웃시 이전에 설정했던 알람을 취소하도록 수정

--- presentation/src/main/java/com/strayalphaca/presentation/screens/settings/home/SettingsHomeViewModel.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/screens/settings/home/SettingsHomeViewModel.kt
@@ -2,6 +2,9 @@ package com.strayalphaca.presentation.screens.settings.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.strayalphaca.presentation.alarm.TrailyAlarmManager
+import com.strayalphaca.travel_diary.domain.alarm.model.AlarmInfo
+import com.strayalphaca.travel_diary.domain.alarm.usecase.UseCaseSetAlarmInfo
 import com.strayalphaca.travel_diary.domain.auth.usecase.UseCaseClearToken
 import com.strayalphaca.travel_diary.domain.auth.usecase.UseCaseGetAccessToken
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +18,9 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsHomeViewModel @Inject constructor(
     private val useCaseGetAccessToken: UseCaseGetAccessToken,
-    private val useCaseClearToken: UseCaseClearToken
+    private val useCaseClearToken: UseCaseClearToken,
+    private val alarmManager: TrailyAlarmManager,
+    private val useCaseSetAlarmInfo: UseCaseSetAlarmInfo
 ) : ViewModel() {
     private val _isLogin = MutableStateFlow(false)
     val isLogin = _isLogin.asStateFlow()
@@ -44,6 +49,10 @@ class SettingsHomeViewModel @Inject constructor(
     fun logout() {
         viewModelScope.launch {
             useCaseClearToken()
+
+            useCaseSetAlarmInfo(AlarmInfo(alarmOn = false, hour = 20, minute = 0))
+            alarmManager.cancelAlarm()
+
             _navigateToIntroEvent.emit(true)
         }
     }
