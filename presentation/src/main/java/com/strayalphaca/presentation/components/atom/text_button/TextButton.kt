8a708affa43b77961d4213e650f2346f002f139d
yunsehwan <sh10302@naver.com>
2023-11-30 20:07:25 +0900

    [fix] 텍스트 버튼 클릭시 ripple 효과 대신, 해당 텍스트의 글자 색상을 변경하는 이펙트를 사용하도록 수정

--- presentation/src/main/java/com/strayalphaca/presentation/components/atom/text_button/TextButton.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/components/atom/text_button/TextButton.kt
@@ -2,11 +2,15 @@ package com.strayalphaca.presentation.components.atom.text_button
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,11 +28,17 @@ fun TextButton(
     text : String,
     state : TextButtonState = TextButtonState.ACTIVE,
     onClick : () -> Unit = {},
-    textStyle : TextStyle = MaterialTheme.typography.button
+    textStyle : TextStyle = MaterialTheme.typography.button,
+    interactionSource : MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
+    val isPressed by interactionSource.collectIsPressedAsState()
     Box(
         modifier
-            .clickable(enabled = state != TextButtonState.INACTIVE) {
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                enabled = state != TextButtonState.INACTIVE,
+            ) {
                 onClick()
             },
     ) {
@@ -36,7 +46,11 @@ fun TextButton(
             text = text,
             modifier = Modifier.padding(vertical = 12.dp),
             style = textStyle,
-            color = if (state == TextButtonState.ACTIVE) MaterialTheme.colors.onBackground else Gray4
+            color = if (state == TextButtonState.ACTIVE && !isPressed) {
+                MaterialTheme.colors.onBackground
+            } else {
+                Gray4
+            }
         )
     }
 }
