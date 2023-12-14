da59b0f75d3a9c9d4760bf3e85032ee3cb279377
yunsehwan <sh10302@naver.com>
2023-12-14 11:01:15 +0900

    [fix] ContentIconImage에 press시 tint색상 변경 인터렉션 추가

--- presentation/src/main/java/com/strayalphaca/presentation/screens/diary/component/ContentIconImage.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/screens/diary/component/ContentIconImage.kt
@@ -2,32 +2,42 @@ package com.strayalphaca.presentation.screens.diary.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.strayalphaca.presentation.ui.theme.Gray4
 
 @Composable
 fun ContentIconImage(
     iconId : Int,
     descriptionText : String ?= null,
-    onClick : () -> Unit = {},
-
+    onClick : (() -> Unit)? = null,
+    interactionSource : MutableInteractionSource = remember {MutableInteractionSource()}
 ) {
-    val interactionSource = remember {MutableInteractionSource()}
-    val iconTint = MaterialTheme.colors.onSurface
+    val isPressed by interactionSource.collectIsPressedAsState()
 
     Icon(
         painter = painterResource(id = iconId),
         contentDescription = descriptionText,
-        modifier = Modifier.size(36.dp).clickable(
-            indication = null,
-            interactionSource = interactionSource
-        ) { onClick() },
-        tint = iconTint
+        modifier = Modifier
+            .size(36.dp)
+            .thenIf(
+                condition = onClick != null,
+                other = Modifier.clickable(
+                    indication = null,
+                    interactionSource = interactionSource
+                ) { onClick?.invoke() }
+            ),
+        tint = if (isPressed) Gray4 else MaterialTheme.colors.onSurface
     )
 }
+
+private fun Modifier.thenIf(condition : Boolean, other : Modifier) : Modifier =
+    if (condition) this.then(other) else this
