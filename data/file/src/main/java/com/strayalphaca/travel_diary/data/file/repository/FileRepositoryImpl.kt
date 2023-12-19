7b05bb18b4dd2985800d1be6341fda26f0f04c0e
yunsehwan <sh10302@naver.com>
2023-12-19 16:28:10 +0900

    [fix] FileRepository, FileRepositoryImpl에서 BaseResponse import 위치가 이전 모듈로 설정되어있던 문제 수정

--- data/file/src/main/java/com/strayalphaca/travel_diary/data/file/repository/FileRepositoryImpl.kt
+++ data/file/src/main/java/com/strayalphaca/travel_diary/data/file/repository/FileRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.strayalphaca.travel_diary.data.file.repository
 
-import com.strayalphaca.domain.model.BaseResponse
+import com.strayalpaca.travel_diary.core.domain.model.BaseResponse
 import com.strayalphaca.travel_diary.domain.file.model.FileInfo
 import com.strayalphaca.travel_diary.domain.file.repository.FileRepository
 import javax.inject.Inject
@@ -9,7 +9,7 @@ import javax.inject.Singleton
 @Singleton
 class FileRepositoryImpl @Inject constructor() : FileRepository {
     override suspend fun uploadFile(fileInfo: FileInfo): BaseResponse<String> {
-        return BaseResponse.Success<String>(data = "string")
+        return BaseResponse.Success(data = "string")
     }
 
 }
