4fbd3ce2b5ad7c5a2db30af9f5a57aa5ccb8e88f
yunsehwan <sh10302@naver.com>
2023-12-21 21:48:40 +0900

    [feature] notification 클릭시 일지 작성 화면으로 이동 기능 구현

--- presentation/src/main/java/com/strayalphaca/presentation/models/Route.kt
+++ presentation/src/main/java/com/strayalphaca/presentation/models/Route.kt
@@ -4,13 +4,14 @@ import com.strayalphaca.presentation.R
 
 sealed class Route(val uri : String?, val screenNameId : Int) {
     object Calendar : Route(uri = "${scheme}://${host}?calendar", screenNameId = R.string.calendar_page)
-    object Map : Route(uri = "${scheme}://${host}?map", screenNameId = R.string.map_page)
     object DiaryWrite : Route(uri = "${scheme}://${host}?diary_write", screenNameId = R.string.diary_write_page)
     object Null : Route(uri = null, screenNameId = R.string.not_select)
     companion object {
-        val pushAlarmTargetList = listOf(
-            Calendar, Map, DiaryWrite, Null
-        )
+        val pushAlarmTargetList: List<Route> by lazy {
+            listOf(
+                Calendar, DiaryWrite, Null
+            )
+        }
 
         private const val host = "notification"
         private const val scheme = "traily"
@@ -18,7 +19,6 @@ sealed class Route(val uri : String?, val screenNameId : Int) {
         fun getInstanceByUriString(uriString : String?) : Route {
             return when (uriString) {
                 Calendar.uri -> Calendar
-                Map.uri -> Map
                 DiaryWrite.uri -> DiaryWrite
                 else -> Null
             }
